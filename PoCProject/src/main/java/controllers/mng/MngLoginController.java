037846caea7cec21bb601352c4a3673f728278bb
anhNT139 <71156317+anhNT139@users.noreply.github.com>
2020-12-07 21:42:39 +0700

    Update MngLoginController.java

--- PoCProject/src/main/java/controllers/mng/MngLoginController.java
+++ PoCProject/src/main/java/controllers/mng/MngLoginController.java
@@ -13,28 +13,29 @@ import java.sql.*;
  * @author macbookpro
  */
 public class MngLoginController {
+    private static boolean check = false;
+    
+    public static boolean getCheck(){
+        return check;
+    }
 
     public boolean mngLogin(String mngUsername, String mngPassword) {
         try {
-            Connection connection = MysqlConnection.getMysqlConnection();
-            Statement statement = connection.createStatement();
-            String query = "SELECT MNGUSERNAME, MNGPASSWORD FROM MANAGER WHERE MNGUSERNAME = '" + mngUsername + "'";
-            ResultSet rs = statement.executeQuery(query);
-            if (rs.next()) {
-                rs.close();
-                return true;
-            } else {
-                rs.close();
-                return false;
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-            return false;
-        }
+                    Connection conn = DB.getConn();
+                    Statement st = conn.createStatement();
+                    ResultSet rs = st.executeQuery("select * from account");
+                    
+                    while(rs.next()){
+                        if(rs.getString("username").equals(user) && rs.getString("password").equals(pass))
+                            check = true;
+                    }
+                    if(check){
+                        JOptionPane.showMessageDialog(null, "Dang nhap thanh cong");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Tai khoan khong ton tai", "Warning", JOptionPane.WARNING_MESSAGE);
+                    }   
+            } catch(SQLException e){
+        }       
     }
     
-//    public static void main(String args[]) {
-//        MngLoginController mng = new MngLoginController();
-//        System.out.println(mng.mngLogin("admin", "admin"));
-//    }
 }
