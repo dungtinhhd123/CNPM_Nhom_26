5a4320e5cd0b2ee0b3bd1c1558faa428716fe300
dungtinhhd123 <42860701+dungtinhhd123@users.noreply.github.com>
2020-12-08 13:48:52 +0700

    Add files via upload

--- PoCProject/src/main/java/controllers/mng/PheDuyetYeuCauNguoiDung.java
+++ PoCProject/src/main/java/controllers/mng/PheDuyetYeuCauNguoiDung.java
@@ -10,6 +10,9 @@ import models.DonYeuCau;
 import java.util.List;
 import java.util.ArrayList;
 import java.sql.*;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 /**
  *
@@ -44,47 +47,84 @@ public class PheDuyetYeuCauNguoiDung {
         return listDonYeuCau;
     }
 
+    public List<DonYeuCau> getListDonYeuCau() {
+        return listDonYeuCau;
+    }
+
+    public JTable danhSachChuaPheDuyetTable(List<DonYeuCau> listDonYeuCau) {
+        int row = listDonYeuCau.size();
+        String data[][] = new String[listDonYeuCau.size()][6];
+        int i = 0;
+        for (DonYeuCau donYeuCau : listDonYeuCau) {
+            data[i][0] = "" + donYeuCau.getRequestID();
+            data[i][1] = donYeuCau.getUserUsername();
+            data[i][2] = donYeuCau.getStartTime();
+            data[i][3] = donYeuCau.getFinishTime();
+            data[i][4] = donYeuCau.getRequestRoom();
+            data[i++][5] = donYeuCau.getRequestDescription();
+        }
+
+        // Tên các cột của bảng danh sách các yêu cầu
+        String columns[] = {"Số hiệu yêu cầu", "TĐN người dùng", "Thời gian bắt đầu", "Thời gian kết thúc", "Phòng yêu cầu", "Mô tả yêu cầu"};
+
+        JTable jTable =  new JTable(data, columns) {           
+            public boolean editCellAt(int row, int column, java.util.EventObject e) {
+                return false;
+            }
+        };
+        return jTable;
+    }
+
     public static void main(String args[]) {
         PheDuyetYeuCauNguoiDung pheDuyetYeuCau = new PheDuyetYeuCauNguoiDung();
-        for (DonYeuCau show : pheDuyetYeuCau.getDanhSachChuaPheDuyet()) {
-            System.out.println(show.getUserUsername());
-            System.out.println(show.getStartTime());
-            System.out.println(show.getFinishTime());
-            System.out.println(show.getRequestRoom());
-            System.out.println(show.getRequestDescription());
-            System.out.println(show.getRequestID());         
-        }
-//        
-//        pheDuyetYeuCau.pheDuyetYeuCau(6);
-//        
+        //        for (DonYeuCau show : pheDuyetYeuCau.getDanhSachChuaPheDuyet()) {
+        //            System.out.println(show.getUserUsername());
+        //            System.out.println(show.getStartTime());
+        //            System.out.println(show.getFinishTime());
+        //            System.out.println(show.getRequestRoom());
+        //            System.out.println(show.getRequestDescription());
+        //            System.out.println(show.getRequestID());         
+        //        }
+        //        
+        //        pheDuyetYeuCau.pheDuyetYeuCau(6);
+        //        
+        JFrame jFrame = new JFrame();
+        jFrame.setLayout(null);
+        jFrame.setVisible(true);
+        JTable jTable1 = pheDuyetYeuCau.danhSachChuaPheDuyetTable(pheDuyetYeuCau.getDanhSachChuaPheDuyet());
+        jTable1.setBounds(10, 10, 500, 500);
+        JScrollPane jScrollPane1 = new JScrollPane(jTable1);
+        jScrollPane1.setBounds(100, 100, 500, 300);
+        jFrame.add(jScrollPane1);
+        jFrame.setSize(1000, 1000);
+        
+        
+
     }
 
     /* 
      * Tìm xem trong danh sách các yêu cầu chưa phê duyệt, lấy ra yêu cầu có REQUESTID = requestID,
-     * phê duyệt yêu cầu này và đưa nó ra khỏi danh sách chờ, nếu như thiết lập trạng thái cho
-     * yêu cầu này là "để sau" (later) thì không cần đưa yêu cầu ra khỏi danh sách chờ.
+     * phê duyệt yêu cầu này và đưa nó ra khỏi danh sách chờ
      */
-    public void pheDuyetYeuCau(int requestID) {
+    public void loaiBoYeuCauDaPheDuyet(int requestID) {
         List<DonYeuCau> temp = getDanhSachChuaPheDuyet();
         for (DonYeuCau pheDuyet : temp) {
             if (pheDuyet.getRequestID() == requestID) {
-                if (!ketQuaPheDuyet(requestID, "Tu choi", "admin", "Dang ban", 0).equals("Để sau")) {
-                    temp.remove(pheDuyet);
-                };
+                temp.remove(pheDuyet);
                 break;
-            }         
+            }
         }
     }
 
-    public String ketQuaPheDuyet(int requestID, String state, String mngUsername, String note, int fee) {
-        try {                        
+    public boolean ketQuaPheDuyet(int requestID, String state, String mngUsername, String note, int fee) {
+        try {
             if (!state.equals("Để sau")) {
                 Connection connection = MysqlConnection.getMysqlConnection();
                 String prequery = "INSERT INTO APPROVE(MNGUSERNAME,\n"
                         + "REQUESTID,\n"
                         + "STATE ,\n"
                         + "NOTE ,\n"
-                        + "FEE) \n" 
+                        + "FEE) \n"
                         + " VALUES(?, ?, ?, ?, ?)";
                 PreparedStatement pstatement = connection.prepareStatement(prequery);
                 pstatement.setString(1, mngUsername);
@@ -92,15 +132,12 @@ public class PheDuyetYeuCauNguoiDung {
                 pstatement.setString(3, state);
                 pstatement.setString(4, note);
                 pstatement.setInt(5, fee);
-                pstatement.executeUpdate();                
-                // Sau khi phê duyệt yêu cầu xong thì loại yêu cầu đó ra khỏi danh sách yêu cầu.
-                prequery = "DELETE FROM REQUEST WHERE REQUESTID = " + requestID;
-                Statement statement = connection.createStatement();
-                statement.executeUpdate(prequery);
+                pstatement.executeUpdate();
             }
         } catch (Exception e) {
-            System.out.println(e);            
+            System.out.println(e);
+            return false;
         }
-        return state;
+        return true;
     }
 }
