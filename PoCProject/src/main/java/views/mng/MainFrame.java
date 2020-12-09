730136230ad16ce5a9df4304094631bae25b1e50
anhNT139 <71156317+anhNT139@users.noreply.github.com>
2020-12-09 17:13:33 +0700

    Update MainFrame.java

--- PoCProject/src/main/java/views/mng/MainFrame.java
+++ PoCProject/src/main/java/views/mng/MainFrame.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package views;
+package views.mng;
 
-import controller.*;
+import controllers.mng.*;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
