c02678d2e85797cd305d6504723800b854fa6911
anhNT139 <71156317+anhNT139@users.noreply.github.com>
2020-12-09 21:08:39 +0700

    Update ThemSuKienFrame.java

--- PoCProject/src/main/java/views/mng/ThemSuKienFrame.java
+++ PoCProject/src/main/java/views/mng/ThemSuKienFrame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package views;
+package views.mng;
 import models.LenLichSuKien;
 import controller.LenLichSuKienController;
 import java.time.LocalDate;
